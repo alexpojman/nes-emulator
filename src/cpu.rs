254bf752f86b6f3464a4b0114009b5e493ca96db
Alex Pojman <pojman.alex@gmail.com>
2022-04-03 13:16:57 -0500

    Add implmentation for running snake

--- src/cpu.rs
+++ src/cpu.rs
@@ -39,7 +39,7 @@ pub struct CPU {
     memory: [u8; 0xFFFF],
 }
 
-trait Mem {
+pub trait Mem {
     fn mem_read(&self, addr: u16) -> u8;
 
     fn mem_write(&mut self, addr: u16, data: u8);
@@ -274,9 +274,18 @@ impl CPU {
     }
 
     pub fn run(&mut self) {
+        self.run_with_callback(|_| {});
+    }
+
+    pub fn run_with_callback<F>(&mut self, mut callback: F)
+    where
+        F: FnMut(&mut CPU),
+    {
         let ref opcodes: HashMap<u8, &'static opcodes::OpCode> = *opcodes::OPCODES_MAP;
 
         loop {
+            callback(self);
+
             let current_code = self.mem_read(self.program_counter);
             self.program_counter += 1;
             let program_counter_state = self.program_counter;
@@ -503,7 +512,7 @@ impl CPU {
 
                 /* TYA */
                 0x98 => self.tya(),
-                
+
                 _ => todo!(""),
             }
 
